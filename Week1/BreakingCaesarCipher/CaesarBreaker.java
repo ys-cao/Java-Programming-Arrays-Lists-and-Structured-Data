abae5801f3e53c6023578e5bb605d8917c604b9d
Yishuai Cao <74084410+ys-cao@users.noreply.github.com>
2021-01-12 22:31:32 -0500

    Create CaesarBreaker.java

---
+++ Week1/BreakingCaesarCipher/CaesarBreaker.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of CaesarBreaker here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CaesarBreaker {
+    public int countLetters() {
+
+    }
+
+    public int maxIndex() {
+
+    }
+
+    public String decrypt() {
+
+    }
+
+    public void testDecrypt() {
+
+    }
+
+    public String halfOfString(String message, int start) {
+
+    }
+
+    public int getKey(String s) {
+
+    }
+
+    public void decrpytTwoKeys (String encrypted) {
+
+    }
+}
