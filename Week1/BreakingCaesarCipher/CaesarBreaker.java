a030eb548368fb3fccbfbb9dd7fcd5ea915711c5
Yishuai Cao <74084410+ys-cao@users.noreply.github.com>
2021-01-13 21:15:46 -0500

    Update CaesarBreaker.java

--- Week1/BreakingCaesarCipher/CaesarBreaker.java
+++ Week1/BreakingCaesarCipher/CaesarBreaker.java
@@ -1,32 +1,34 @@
-import edu.duke.*;
-// import java.lang.*;
 
-public class CaesarBreaker() {
+/**
+ * Write a description of CaesarBreaker here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import edu.duke.*;
+
+public class CaesarBreaker {
     public int[] countLetters(String s) {
-        String alphabet = "abcdefghcjklmnopqrstuvwxyz";
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
         int[] lettersCount = new int[26];
-        int index = 0;
         for (int i = 0; i < s.length(); i++) {
             char currChar = Character.toLowerCase(s.charAt(i));
-            index = alphabet.indexOf(currChar);
+            int index = alphabet.indexOf(currChar);
             if (index != -1) {
-                lettersCount[index]++;
+                lettersCount[index] += 1;
             }
         }
         return lettersCount;
     }
 
     public int maxIndex(int[] countLetters) {
-        int temp = 0;
-        int index = 0;
+        int maxDex = 0;
         for (int i = 0; i < countLetters.length; i++) {
-            int currCount = countLetters[i];
-            if (temp < currCount) {
-                temp = currCount;
-                index = i;
+            if (countLetters[i] > countLetters[maxDex]) {
+                maxDex = i;
             }
         }
-        return index;
+        return maxDex;
     }
 
     public String decrypt(String encrypted) {
@@ -37,11 +39,11 @@ public class CaesarBreaker() {
         if (maxDex < 4) {
             dkey = 26 - (4 - maxDex);
         }
-        return cc.encrypt(encrypted, 26 - dkey);
+        return cc.encrypt(encrypted, 26-dkey);
     }
 
     public void testDecrypt() {
-        String encrypted = "";
+        String encrypted = "CFOPQ IBDFLK XQQXZH BXPQ CIXKH!";
         String decrypted = decrypt(encrypted);
         System.out.println("Decrypted: " + decrypted);
     }
@@ -49,17 +51,26 @@ public class CaesarBreaker() {
     public String halfOfString(String message, int start) {
         StringBuilder sb = new StringBuilder();
         if (start == 0 || start == 1) {
-            for (int = start; start <= message.length; start += 2) {
-                char currChar = message.charAt(start);
+            for (int i = start; i < message.length(); i += 2) {
+                char currChar = message.charAt(i);
                 sb.append(currChar);
             }
         } else {
-            System.out.println("Please use 0 or 1 for start value."")
+            System.out.println("Please use 0 or 1 for start value.");
         }
-        String str = sb.toString();
-        return str;
+        return sb.toString();
     }
-
+    
+    public void testHalfOfString() {
+        String message = "Just a test string with lots of eeeeeeeeeeeeeeeees";
+        int start = 0;
+        System.out.println("Halved message = " + halfOfString(message, start));
+        
+        message = "Just a test string with lots of eeeeeeeeeeeeeeeees";
+        start = 1;
+        System.out.println("Halved message = " + halfOfString(message, start));
+    }
+    
     public int getKey(String s) {
         int[] freqs = countLetters(s);
         int maxDex = maxIndex(freqs);
@@ -67,18 +78,19 @@ public class CaesarBreaker() {
         if (maxDex < 4) {
             dkey = 26 - (4 - maxDex);
         }
+        
         return dkey;
     }
-
-    public void decrpytTwoKeys (String encrypted) {
+    
+    public void decrpytTwoKeys () {
+        String encrypted = "Gwpv c vbuq pvokki yfve iqqu qc bgbgbgbgbgbgbgbgbu";
         String s1 = halfOfString(encrypted, 0);
         String s2 = halfOfString(encrypted, 1);
         int key1 = getKey(s1);
         int key2 = getKey(s2);
         System.out.println("Found Two Keys: " + key1 + " & " + key2 +".");
-
-        CaesarCipher cc = new CaesarCiper();
-        String decrypted = cc.encryptTwoKeys(encrypted, key1, key2);
+        CaesarCipher cc = new CaesarCipher();
+        String decrypted = cc.encryptTwoKeys(encrypted, 26-key1, 26-key2);
         System.out.println("Decrypted: " + decrypted);
     }
 }
