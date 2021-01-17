5ade490e5132c59f4d4c4be7092a888eeb882161
Yishuai Cao <74084410+ys-cao@users.noreply.github.com>
2021-01-16 22:18:28 -0500

    Create Tester.java

---
+++ Week3/ReadingWebLogs/Tester.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class Tester here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import java.util.*;
+
+public class Tester
+{
+    public void testLogEntry() {
+        LogEntry le = new LogEntry("1.2.3.4", new Date(), "example request", 200, 500);
+        System.out.println(le);
+        LogEntry le2 = new LogEntry("1.2.100.4", new Date(), "example request 2", 300, 400);
+        System.out.println(le2);
+    }
+    
+    public void testLogAnalyzer() {
+        LogAnalyzer log = new LogAnalyzer();
+        log.readFile("short-test_log");
+        log.printAll();
+    }
+}
