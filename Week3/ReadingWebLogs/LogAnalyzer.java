faf07b4b4f129957f6a2a19a655531b6aaceefed
Yishuai Cao <74084410+ys-cao@users.noreply.github.com>
2021-01-16 22:13:15 -0500

    Create LogAnalyzer.java

---
+++ Week3/ReadingWebLogs/LogAnalyzer.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class LogAnalyzer here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import java.util.*;
+import edu.duke.*;
+
+public class LogAnalyzer
+{
+     private ArrayList<LogEntry> records;
+     
+     public LogAnalyzer() {
+         records = new ArrayList<LogEntry>();
+     }
+        
+     public void readFile(String filename) {
+         FileResource fr = new FileResource();
+         for (String line : fr.lines()) {
+             LogEntry log = WebLogParser.parseEntry(line);
+             records.add(log);
+         }
+     }
+        
+     public void printAll() {
+         for (LogEntry le : records) {
+             System.out.println(le);
+         }
+     }
+     
+     
+}
